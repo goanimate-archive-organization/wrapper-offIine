8d509ef45e9b39949cd787189d6056ded9c2f9f3
2Virus4u <69337817+zoccorus@users.noreply.github.com>
2021-05-13 00:03:49 +0800

    ...

--- update_wrapper.bat
+++ update_wrapper.bat
@@ -13,17 +13,46 @@ echo Otherwise, press any key.
 echo:
 set /p GITPULL= Option:
 if "%GITPULL%"=="1" (
-	if not exist ".git" (
-		echo No .git folder, eh?
-		PING -n 4 127.0.0.1>nul
-		echo You were supposed to download it using THE INSTALLER^!
-		PING -n 4 127.0.0.1>nul
-		set GITPULL=
-	) else (
 		cls
+		echo Checking for Git installation...
+		echo:
+		if not exist "%PROGRAMFILES%\Git" (
+			if not exist "%PROGRAMFILES(X86)%\Git" (
+				echo Could not detect Git in Program Files folders.
+				echo:
+				echo Checking via command-line testing...
+				for /f "delims=" %%i in ('git^>nul') do set output=%%i
+				echo:
+				IF "%output%" EQU "" (
+					echo Okay, there's no sign of Git ANYWHERE on this computer.
+					PING -n 4 127.0.0.1>nul
+					echo That means YOU MUST HAVE INSTALLED THIS INCORRECTLY!
+					PING -n 4 127.0.0.1>nul
+					set GITPULL=""
+				) else (
+					echo The command-line worked, therefore Git is installed.
+					echo:
+				)
+			)
+		) else (
+			echo Git was detected in one of the Program Files folders, therefore it is installed.
+			echo:
+		)
+		echo Saving custom settings in temporary file...
+		pushd utilities
+		copy config.bat tmpcfg.bat>nul
+		popd
+		echo:
 		echo Pulling latest version of repository from GitHub through Git...
 		PING -n 4 127.0.0.1>nul
+		echo:
 		call git pull
+		echo:
+		echo Deleting config.bat from repository and replacing it with user's copy...
+		pushd utilities
+		del config.bat
+		ren tmpcfg.bat config.bat
+		popd
 		echo:
 		echo Latest version of repository pulled^!
 		echo:
