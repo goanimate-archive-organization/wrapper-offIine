e9489467c28cec1c42641b264d28db1ead439567
2Virus4u <69337817+zoccorus@users.noreply.github.com>
2021-05-28 17:48:39 +0800

    ...

--- utilities/config.bat
+++ utilities/config.bat
@@ -1,3 +1,4 @@
+
 :: Wrapper: Offline Config
 :: This file is modified by settings.bat. It is not organized, but comments for each setting have been added.
 :: You should be using settings.bat, and not touching this. Offline relies on this file remaining consistent, and it's easy to mess that up.
@@ -8,10 +9,10 @@ if "%SUBSCRIPT%"=="" ( start notepad.exe "%CD%\%~nx0" & exit )
 endlocal
 
 :: Shows exactly Offline is doing, and never clears the screen. Useful for development and troubleshooting. Default: n
-set VERBOSEWRAPPER=y
+set VERBOSEWRAPPER=n
 
 :: Won't check for dependencies (flash, node, etc) and goes straight to launching. Useful for speedy launching post-install. Default: n
-set SKIPCHECKDEPENDS=y
+set SKIPCHECKDEPENDS=n
 
 :: Won't install dependencies, regardless of check results. Overridden by SKIPCHECKDEPENDS. Mostly useless, why did I add this again? Default: n
 set SKIPDEPENDINSTALL=n
@@ -39,7 +40,7 @@ set CEPSTRAL=n
 set INCLUDEDBASILISK=n
 
 :: Makes it so both the settings and the Wrapper launcher shows developer options. Default: n
-set DEVMODE=y
+set DEVMODE=n
 
 :: Tells settings.bat which port the frontend is hosted on. (If changed manually, you MUST also change the value of "SERVER_PORT" to the same value in wrapper\env.json) Default: 4343
 set PORT=4343
