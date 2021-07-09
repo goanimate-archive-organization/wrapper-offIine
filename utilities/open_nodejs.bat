4c63376e7f8b1ebae9f2b3a15f84bc7666113327
windowsme <69337817+zoccorus@users.noreply.github.com>
2021-07-09 21:18:13 +0800

    Update open_nodejs.bat

--- utilities/open_nodejs.bat
+++ utilities/open_nodejs.bat
@@ -13,9 +13,19 @@ cls
 goto start
 ) else (
 echo Uh-oh!
-echo Either Node.js has crashed or you don't have it installed.
+echo You Have Been Crashed!!!
+echo How to fix wrapper offline
+echo 1. go to wrapper
+echo 2. select _CACHE and _SAVED folder and move to desktop.
+echo 3. Reinstall or Repair Wrapper Offline
+echo 4. go to settings.bat and switch on verbose mode
+echo And done! you will no longer crash forever! U CAN USE IT!
+echo issues? Contact on discord.
+echo zoccorus#9939
+echo NOTE: Either Node.js has crashed or you don't have it installed.
 echo If Node.js crashed, please send the error in the GitHub issues page.
-echo If you don't have Node.js, install it in the utilities folder.
+echo If you don't have Node.js, install it in the utilities/installers or [Node.js LTS](https://nodejs.org/en/download/) or [Node.js Current](https://nodejs.org/en/download/current/).
+
 echo:
 echo If you saw an error that says "MODULE_NOT_FOUND",
 echo go in the utilities folder and run module_installer.bat.
