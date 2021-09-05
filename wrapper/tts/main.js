e0a750d57ce866134f91784413a675ef6d24b7f6
windowsme <69337817+zoccorus@users.noreply.github.com>
2021-09-05 08:35:12 +0800

    update

--- wrapper/tts/main.js
+++ wrapper/tts/main.js
@@ -19,77 +19,87 @@ module.exports = (voiceName, text) => {
 		const voice = voices[voiceName];
 		switch (voice.source) {
 			case "polly+azure": {
-				var req = https.request({
-					hostname: "voicemaker.in",
-					port: "443",
-					path: "/voice/standard",
-					method: "POST",
-					headers: {
-						"content-type": "application/json",
-						"cookie": "__stripe_mid=0a37ccc8-cc13-474d-97da-aa0a5f9f47398bdf7a; __cfduid=d022698980296baa7fa7313e4cf44f5631616037532; connect.sid=s%3AgvSXg1g2nPq-07vxD25_STJDMnFVSydC.CnUtht28g%2BsJ8NMGhRQeB0oBrs5W5kCD8pOI4MPJeDw; __stripe_sid=2dd1624f-ae48-472a-b598-b7d5cab469ecc72729",
-						"csrf-token": "",
-						"origin": "https://voicemaker.in",
-						"referer": "https://voicemaker.in/",
-						"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.101 Safari/537.36",
-						"x-requested-with": "XMLHttpRequest",
+				var req = https.request(
+					{
+						hostname: "voicemaker.in",
+						port: "443",
+						path: "/voice/standard",
+						method: "POST",
+						headers: {
+							"content-type": "application/json",
+							cookie:
+								"__stripe_mid=0a37ccc8-cc13-474d-97da-aa0a5f9f47398bdf7a; __cfduid=d022698980296baa7fa7313e4cf44f5631616037532; connect.sid=s%3AgvSXg1g2nPq-07vxD25_STJDMnFVSydC.CnUtht28g%2BsJ8NMGhRQeB0oBrs5W5kCD8pOI4MPJeDw; __stripe_sid=2dd1624f-ae48-472a-b598-b7d5cab469ecc72729",
+							"csrf-token": "",
+							origin: "https://voicemaker.in",
+							referer: "https://voicemaker.in/",
+							"user-agent":
+								"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.101 Safari/537.36",
+							"x-requested-with": "XMLHttpRequest",
+						},
 					},
-				},
-				(r) => {
-					var buffers = [];
-					r.on("data", (b) => buffers.push(b));
-					r.on("end", () => {
-						var json = JSON.parse(Buffer.concat(buffers));
-						get(`https://voicemaker.in/${json.path}`).then(res).catch(rej);
-					});
-					r.on("error", rej);
-				});
-				req.write(`{"Engine":"${voice.speech_engine}","Provider":"${voice.ai}","OutputFormat":"mp3","VoiceId":"${voice.arg}","LanguageCode":"${voice.language}-${voice.country}","SampleRate":"${voice.samplerate}","effect":"default","master_VC":"advanced","speed":"0","master_volume":"0","pitch":"0","Text":"${text}","TextType":"text","fileName":""}`);
+					(r) => {
+						var buffers = [];
+						r.on("data", (b) => buffers.push(b));
+						r.on("end", () => {
+							var json = JSON.parse(Buffer.concat(buffers));
+							get(`https://voicemaker.in/${json.path}`).then(res).catch(rej);
+						});
+						r.on("error", rej);
+					}
+				);
+				req.write(
+					`{"Engine":"${voice.speech_engine}","Provider":"${voice.ai}","OutputFormat":"mp3","VoiceId":"${voice.arg}","LanguageCode":"${voice.language}-${voice.country}","SampleRate":"${voice.samplerate}","effect":"default","master_VC":"advanced","speed":"0","master_volume":"0","pitch":"0","Text":"${text}","TextType":"text","fileName":""}`
+				);
 				req.end();
 				break;
 			}
 			/* WARNING: NUANCE TTS API HAS BACKGROUND MUSIC */
-            case "nuance": {
-                var q = qs.encode({
-                    voice_name: voice.arg,
-                    speak_text: text,
-                });
-                https.get({
-                        host: "voicedemo.codefactoryglobal.com",
-                        path: `/generate_audio.asp?${q}`,
-                    },
-                    (r) => {
-                        var buffers = [];
-                        r.on("data", (d) => buffers.push(d));
-                        r.on("end", () => res(Buffer.concat(buffers)));
-                        r.on("error", rej);
-                    }
-                );
-                break;
-            }
+			case "nuance": {
+				var q = qs.encode({
+					voice_name: voice.arg,
+					speak_text: text,
+				});
+				https.get(
+					{
+						host: "voicedemo.codefactoryglobal.com",
+						path: `/generate_audio.asp?${q}`,
+					},
+					(r) => {
+						var buffers = [];
+						r.on("data", (d) => buffers.push(d));
+						r.on("end", () => res(Buffer.concat(buffers)));
+						r.on("error", rej);
+					}
+				);
+				break;
+			}
 			case "cepstral": {
-				https.get('https://www.cepstral.com/en/demos', r => {
-					const cookie = r.headers['set-cookie'];
+				https.get("https://www.cepstral.com/en/demos", (r) => {
+					const cookie = r.headers["set-cookie"];
 					var q = qs.encode({
 						voiceText: text,
 						voice: voice.arg,
 						createTime: 666,
 						rate: 170,
 						pitch: 1,
-						sfx: 'none',
+						sfx: "none",
 					});
 					var buffers = [];
-					var req = https.get({
-						host: 'www.cepstral.com',
-						path: `/demos/createAudio.php?${q}`,
-						headers: { Cookie: cookie },
-						method: 'GET',
-					}, r => {
-						r.on('data', b => buffers.push(b));
-						r.on('end', () => {
-							var json = JSON.parse(Buffer.concat(buffers));
-							get(`https://www.cepstral.com${json.mp3_loc}`).then(res).catch(rej);
-						})
-					});
+					var req = https.get(
+						{
+							host: "www.cepstral.com",
+							path: `/demos/createAudio.php?${q}`,
+							headers: { Cookie: cookie },
+							method: "GET",
+						},
+						(r) => {
+							r.on("data", (b) => buffers.push(b));
+							r.on("end", () => {
+								var json = JSON.parse(Buffer.concat(buffers));
+								get(`https://www.cepstral.com${json.mp3_loc}`).then(res).catch(rej);
+							});
+						}
+					);
 				});
 				break;
 			}
@@ -112,8 +122,8 @@ module.exports = (voiceName, text) => {
 						host: "cache-a.oddcast.com",
 						path: `/tts/gen.php?${q}`,
 						headers: {
-							"Referer": "https://www.oddcast.com/",
-							"Origin": "https://www.oddcast.com/",
+							Referer: "https://www.oddcast.com/",
+							Origin: "https://www.oddcast.com/",
 							"User-Agent":
 								"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36",
 						},
@@ -239,43 +249,76 @@ module.exports = (voiceName, text) => {
 				);
 				break;
 			} */
-            case "acapela": {
-                var buffers = [];
-                var req = https.request({
-                        hostname: "acapela-box.com",
-                        path: "/AcaBox/dovaas.php",
-                        method: "POST",
-                        headers: {
-							"Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
-							"Cookie": "AcaBoxLogged=logged; AcaBoxUsername=acaboxuserkeeg; acabox=5v8gucusuhq022ffn4gb9rops5; AcaBoxFirstname=d",
-							"Origin": "https://acapela-box.com",
-							"Referer": "https://acapela-box.com/AcaBox/index.php",
-							"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.101 Safari/537.36",
-							"X-Requested-With": "XMLHttpRequest",
-                        },
-                    },
-                    (r) => {
-                        r.on("data", (b) => buffers.push(b));
-                        r.on("end", () => {
-                            var json = JSON.parse(Buffer.concat(buffers));
-							get(`${json.snd_url}`).then(res).catch(rej);
-                        });
-                    }
-                );
-                req.write(qs.encode({
-                    text: text,
-                    voice: voice.arg,
-					listen: 1,
-					format: "MP3",
-					codecMP3: 1,
-					spd: 180,
-					vct: 100,
-					byline: 0,
-					ts: 666
-                }));
-                req.end();
-                break;
-            }
+			case "acapela": {
+				var buffers = [];
+				var acapelaArray = [];
+				for (var c = 0; c < 15; c++) acapelaArray.push(~~(65 + Math.random() * 26));
+				var email = `${String.fromCharCode.apply(null, acapelaArray)}@gmail.com`;
+				var req = https.request(
+					{
+						hostname: "acapelavoices.acapela-group.com",
+						path: "/index/getnonce",
+						method: "POST",
+						headers: {
+							"Content-Type": "application/x-www-form-urlencoded",
+						},
+					},
+					(r) => {
+						r.on("data", (b) => buffers.push(b));
+						r.on("end", () => {
+							var nonce = JSON.parse(Buffer.concat(buffers)).nonce;
+							var req = http.request(
+								{
+									hostname: "acapela-group.com",
+									port: "8080",
+									path: "/webservices/1-34-01-Mobility/Synthesizer",
+									method: "POST",
+									headers: {
+										"Content-Type": "application/x-www-form-urlencoded",
+									},
+								},
+								(r) => {
+									var buffers = [];
+									r.on("data", (d) => buffers.push(d));
+									r.on("end", () => {
+										const html = Buffer.concat(buffers);
+										const beg = html.indexOf("&snd_url=") + 9;
+										const end = html.indexOf("&", beg);
+										const sub = html.subarray(beg, end).toString();
+										http.get(sub, (r) => {
+											r.on("data", (d) => buffers.push(d));
+											r.on("end", () => {
+												res(Buffer.concat(buffers));
+											});
+										});
+									});
+									r.on("error", rej);
+								}
+							);
+							req.end(
+								qs.encode({
+									req_voice: voice.arg,
+									cl_pwd: "",
+									cl_vers: "1-30",
+									req_echo: "ON",
+									cl_login: "AcapelaGroup",
+									req_comment: `{"nonce":"${nonce}","user":"${email}"}`,
+									req_text: text,
+									cl_env: "ACAPELA_VOICES",
+									prot_vers: 2,
+									cl_app: "AcapelaGroup_WebDemo_Android",
+								})
+							);
+						});
+					}
+				);
+				req.end(
+					qs.encode({
+						json: `{"googleid":"${email}"`,
+					})
+				);
+				break;
+			}
 			case "acapelaOld": {
 				var q = qs.encode({
 					inputText: base64.encode(text),
@@ -319,29 +362,32 @@ module.exports = (voiceName, text) => {
 				break;
 			}
 			case "wavenet": {
-                var req = https.request({
-                        hostname: "texttospeechapi.wideo.co",
-                        path: "/api/wideo-text-to-speech",
-                        method: "POST",
-                        headers: {
-                            "Content-Type": "application/json",
-							"Origin": "https://texttospeech.wideo.co",
-							"Referer": "https://texttospeech.wideo.co/",
-							"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.101 Safari/537.36",
-                        },
-                    },
-                    (r) => {
+				var req = https.request(
+					{
+						hostname: "texttospeechapi.wideo.co",
+						path: "/api/wideo-text-to-speech",
+						method: "POST",
+						headers: {
+							"Content-Type": "application/json",
+							Origin: "https://texttospeech.wideo.co",
+							Referer: "https://texttospeech.wideo.co/",
+							"User-Agent":
+								"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.101 Safari/537.36",
+						},
+					},
+					(r) => {
 						var buffers = [];
 						r.on("data", (b) => buffers.push(b));
-                        r.on("end", () => {
+						r.on("end", () => {
 							var json = JSON.parse(Buffer.concat(buffers));
 							get(`${json.url}`).then(res).catch(rej);
 						});
 						r.on("error", rej);
-					});
-					req.write(`{"data":{"text":"${text}","speed":1,"voice":"${voice.arg}"}}`);
-					req.end();
-					break;
+					}
+				);
+				req.write(`{"data":{"text":"${text}","speed":1,"voice":"${voice.arg}"}}`);
+				req.end();
+				break;
 			}
 			/*
 			case "acapela": {
@@ -438,10 +484,10 @@ module.exports = (voiceName, text) => {
 						headers: {
 							"content-type": "text/xml",
 							"accept-encoding": "gzip, deflate, br",
-							"origin": "https://www.cereproc.com",
-							"referer": "https://www.cereproc.com/en/products/voices",
+							origin: "https://www.cereproc.com",
+							referer: "https://www.cereproc.com/en/products/voices",
 							"x-requested-with": "XMLHttpRequest",
-							"cookie": "Drupal.visitor.liveDemo=666",
+							cookie: "Drupal.visitor.liveDemo=666",
 						},
 					},
 					(r) => {
